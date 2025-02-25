fd9e76c4c32f499baa6fd97ce7772e38d0bdff86
JesusVicken <jesus.viccken@gmail.com>
2025-02-24 22:40:53 -0300

    feat: ajuste de UI do about

--- pet-shop/src/app/_components/about.tsx
+++ pet-shop/src/app/_components/about.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image"
 import about1Img from "../../../public/about-1.png"
 import about2Img from "../../../public/about-2.png"
+import { Check, MapPin } from "lucide-react"
+import { WhatsappLogo } from "@phosphor-icons/react/dist/ssr"
 
 
 export function About() {
     return (
 
         <section className="bg-[#FDF6ec] py-16">
-            <div className="container mx-auto grid-cols-1 lg:grid-cols-2 gap-12 items-center">
+            <div className="container px-4 mx-auto grid-cols-1 lg:grid-cols-2 gap-12 items-center">
 
                 <div className="relative">
 
@@ -32,6 +34,48 @@ export function About() {
                         />
                     </div>
 
+
+
+                </div>
+
+                <div className="space-y-6 mt-10">
+                    <h2 className="text-4xl font-bold">SOBRE</h2>
+
+                    <p>
+                        No Pet Shop 4 Patas, acreditamos que cada pet é um membro da família e merece todo o amor, carinho e cuidado do mundo! Desde nossa fundação, temos a missão de oferecer os melhores produtos e serviços para garantir a felicidade e o bem-estar do seu amigo de quatro patas.
+                        Contamos com uma equipe apaixonada por animais, sempre pronta para proporcionar um atendimento especial e personalizado. Aqui, você encontra tudo o que seu pet precisa, desde rações premium, brinquedos e acessórios até serviços de banho, tosa e atendimento veterinário de confiança.
+                        Nosso compromisso é com a qualidade, segurança e carinho em cada detalhe. Queremos tornar a vida do seu pet ainda mais saudável e cheia de alegria!
+                    </p>
+                    <ul className="space-y-4">
+                        <li className="flex items-center gap-2">
+                            <Check className="text-red-500" />
+                            Aberto desde 2010
+                        </li>
+                        <li className="flex items-center gap-2">
+                            <Check className="text-red-500" />
+                            Equipe com mais de 10 Veterinários
+                        </li>
+                        <li className="flex items-center gap-2">
+                            <Check className="text-red-500" />
+                            Qualidade de vida para seu pet é a nossa prioridade
+                        </li>
+                    </ul>
+                    <div className="flex gap-2">
+                        <a
+                            href="#"
+                            className="bg-[#E84C3D] text-white flex items-center justify-center w-fit gap-2 px-4 py-2 rounded-md"
+                        >
+                            <WhatsappLogo className="w-5 h-5 text-white" />
+                            Contato via WhatsApp
+                        </a>
+                        <a
+                            href="#"
+                            className="flex items-center justify-center w-fit gap-2 px-4 py-2 rounded-md"
+                        >
+                            <MapPin className="w-5 h-5 text-black" />
+                            Endereço da Loja
+                        </a>
+                    </div>
                 </div>
             </div >
         </section>
